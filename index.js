28af16a2638894dd7e2b40110f1a927868d5e036
Rama <rama.alshaban@gmail.com>
2022-04-19 05:24:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+// Code your solutions in this file
+function writeCards(arr, event){
+    let newArr = []
+    for(let i =0; i<arr.length; i++){
+        newArr[i] = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+    }
+    return  newArr
+
+
+}
+
+function countDown(number){
+    let num = number
+    while(num>=0){
+        console.log(num)
+        num--
+    } 
+    
+}
+countDown(10)
